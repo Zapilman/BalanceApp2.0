2aef7c018e89a195dbb8357b6388aa5495eb1cd2
Юра Здоров <17uraz12@gmail.com>
2020-07-23 15:35:14 +0300

    Добавьте файлы проекта.

---
+++ BalanceApp.BL/Model/Balance.cs
@@ -0,0 +1,40 @@
+﻿namespace BalanceApp.BL.Model
+{
+    /// <summary>
+    /// Balance.
+    /// </summary>
+    public class Balance
+    {
+        /// <summary>
+        /// Name.
+        /// </summary>
+        public string Name { get; }
+        /// <summary>
+        /// Cost.
+        /// </summary>
+        public int Cost { get; }
+        /// <summary>
+        /// To Create a new balance.
+        /// </summary>
+        /// <param name="name"> Name of something. </param>
+        /// <param name="cost"> Cost of something. </param>
+
+        public Balance(string name, int cost)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new System.ArgumentException("Name can not be a null or white space", nameof(name));
+            }
+
+            Name = name;
+            Cost = cost;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}:\t{Cost}";
+        }
+
+
+    }
+}
---
+++ BalanceApp.BL/Model/User.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BalanceApp.BL.Model
+{
+    /// <summary>
+    /// User.
+    /// </summary>
+    public class User
+    {
+        #region Properties
+        /// <summary>
+        /// Name.
+        /// </summary>
+        public string Name { get; }
+        /// <summary>
+        /// Birthday.
+        /// </summary>
+        public DateTime Birthday { get; }
+        /// <summary>
+        /// Balance.
+        /// </summary>
+        public Balance Balance { get; }
+        /// <summary>
+        /// Password.
+        /// </summary>
+        public string Password { get; }
+        /// <summary>
+        /// Login.
+        /// </summary>
+        public string Login { get; }
+
+        /// <summary>
+        /// To create a new user.
+        /// </summary>
+        /// <param name="name"> Name of user. </param>
+        /// <param name="balance"> Balance of user. </param>
+        /// <param name="birthday"> Birthday of user. </param>
+        /// <param name="password"> Password of user. </param>
+        /// <param name="login"> Login of user. </param>
+        #endregion
+        public User(string name,
+                    Balance balance,
+                    DateTime birthday,
+                    string password,
+                    string login)
+        {
+            #region Checking Arguments
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("Name can not be a null or white space", nameof(name));
+            }
+
+            if (balance is null)
+            {
+                throw new ArgumentNullException("Balance can not be a null",nameof(balance));
+            }
+
+            if(birthday < DateTime.Parse("01.01.1900") || birthday > DateTime.Now)
+            {
+                throw new ArgumentException("invalid day of birth", nameof(birthday));
+            }
+
+            if (string.IsNullOrWhiteSpace(password))
+            {
+                throw new ArgumentException("Password can not be a null or white space", nameof(password));
+            }
+
+            if (string.IsNullOrWhiteSpace(login))
+            {
+                throw new ArgumentException("Login can not be a null or white space", nameof(login));
+            }
+            #endregion
+
+            Name = name;
+            Balance = balance;
+            Birthday = birthday;
+            Password = password;
+            Login = login;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
